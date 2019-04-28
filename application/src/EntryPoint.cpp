c53596d6d816f6c7c19990a8ff45be5141098121
03NN30 <n30.email@gmail.com>
2019-04-28 17:02:06 +0200

    updated framework header

--- application/src/EntryPoint.cpp
+++ application/src/EntryPoint.cpp
@@ -47,9 +47,6 @@ void update()
   s2.setProjection(projection_matrix);
 }
 
-#include <GLM/glm.hpp>
-#include <SDL2/SDL.h>
-
 float time_ = 0.0f;
 
 void render() 
@@ -57,9 +54,9 @@ void render()
   s2.use();
   glm::fmat4 model_matrix = glm::fmat4(1.0f);
   //s1.setfMat4("model", model_matrix);
-
+  
   time_ = float(SDL_GetTicks()) / 1000.0f;
-  model_matrix = glm::rotate(model_matrix, 2.0f * time_, glm::fvec3(0.0f, 1.0f, 0.0f));
+  model_matrix = glm::rotate(model_matrix, 2.0f * float(time_), glm::fvec3(0.0f, 1.0f, 0.0f));
   s2.setModel(model_matrix);
   
   
--- matrix/Matrix.h
+++ matrix/Matrix.h
@@ -3,30 +3,55 @@
 
 // application
 #include "matrix/src/Application.h"
-#include "matrix/src/Logger.h"
+
+// debugging
+#ifdef MX_DEBUG
+  #include "matrix/src/Logger.h"
+
+  // testing classes
+  #include "matrix/src/Model_Test.h"
+  #include "matrix/src/Shader_Test.h"
+  #include "matrix/src/Camera_Test.h"
+#endif
+
 #include "matrix/src/Camera.h"
+
+// model
 #include "matrix/src/platform/api/Model.h"
-#include "matrix/src/platform/api/Model_OpenGL.h"
-#include "matrix/src/platform/api/Parser_Wavefront.h"
-// testing classes
-#include "matrix/src/Model_Test.h"
-#include "matrix/src/Shader_Test.h"
-#include "matrix/src/Camera_Test.h"
+#ifdef MX_OPENGL_ACTIVE
+  #include "matrix/src/platform/api/Model_OpenGL.h"
+#elif MX_DIRECTX_ACTIVE
+  #include "matrix/src/platform/api/Model_DirectX.h"
+#endif
+  #include "matrix/src/platform/api/Parser_Wavefront.h"
+
 // layers
 #include "matrix/src/layers/Layer.h"
 #include "matrix/src/layers/LayerStack.h"
+
 // controller
 #include "matrix/src/controller/Controller.h"
+
 // event
 
 // platform
   // api
   #include "matrix/src/platform/api/Shader.h"
-  #include "matrix/src/platform/api/Shader_OpenGL.h"
-  #include "matrix/src/platform/api/Shader_DirectX.h"
+  #ifdef MX_OPENGL_ACTIVE
+    #include "matrix/src/platform/api/Shader_OpenGL.h"
+  #elif MX_DIRECTX_ACTIVE
+    #include "matrix/src/platform/api/Shader_DirectX.h"
+  #endif
+
   // gui
 
   // window
   #include "matrix/src/platform/window/Window.h"
+
+  #ifdef MX_SDL2_ACTIVE
+    #include <SDL2/SDL.h>
+  #elif MX_GLFW_ACTIVE
+    #include <GLFW/glfw3.h>
+  #endif
 
 #endif // MATRIX_H
