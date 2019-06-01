ad526b60bf2c96755de9f33acb136d43ca5883f4
03NN30 <n30.email@gmail.com>
2019-06-01 23:15:11 +0200

    fixed closing app when mouse not hidden

--- matrix/src/event/KeyboardEvent.cpp
+++ matrix/src/event/KeyboardEvent.cpp
@@ -5,11 +5,27 @@ namespace MX
 {
   void KeyboardButtonPressed::handle() 
   {
-    Controller::get().handleKeyPressed(m_KeyCode);
+    int esc_KeyCode;
+    #ifdef MX_SDL2_ACTIVE
+      esc_KeyCode = SDLK_ESCAPE;
+    #elif MX_GLFW_ACTIVE
+      esc_KeyCode = GLFW_KEY_ESCAPE;
+    #endif
+
+    if (Controller::get().m_MouseHidden || m_KeyCode == esc_KeyCode)
+      Controller::get().handleKeyPressed(m_KeyCode);
   }
 
   void KeyboardButtonReleased::handle() 
   {
-    Controller::get().handleKeyReleased(m_KeyCode);
+    int esc_KeyCode;
+    #ifdef MX_SDL2_ACTIVE
+      esc_KeyCode = SDLK_ESCAPE;
+    #elif MX_GLFW_ACTIVE
+      esc_KeyCode = GLFW_KEY_ESCAPE;
+    #endif
+
+    if (Controller::get().m_MouseHidden || m_KeyCode == esc_KeyCode)
+      Controller::get().handleKeyReleased(m_KeyCode);
   }
 }
