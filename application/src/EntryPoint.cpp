c1985b8cf80f3132800d90f22c41dad933ac16f7
03NN30 <n30.email@gmail.com>
2019-06-09 20:01:06 +0200

    updated default setup to use new shaders

--- application/src/EntryPoint.cpp
+++ application/src/EntryPoint.cpp
@@ -15,22 +15,24 @@ void initialize()
   MX_SCENE->push("Saturn", "sphere.obj", "Jupiter");
   MX_SCENE->push("Rock", "rock.obj", "root");
 
+  
+
   MX::Node *jupiter = MX_SCENEGRAPH.search("Jupiter", MX_ROOT);
-  jupiter->setTransform(MX::Y, 0.1f, 1);
-  jupiter->setShader(MX_GET_SHADER("texture"));
+  jupiter->setTransform(MX::Y, 0.3f, 1);
+  jupiter->setShader(MX_GET_SHADER("blinn_phong"));
   jupiter->setTexture(MX_GET_TEXTURE("2k_jupiter.jpg", 1));
 
   MX::Node *saturn = MX_SCENEGRAPH.search("Saturn", MX_ROOT);
   saturn->setTransform(MX::SCALE, 0.4f, 0);
   saturn->setTransform(MX::RIGHT, 5.0f, 0);
-  saturn->setShader(MX_GET_SHADER("texture"));
-  saturn->setTexture(MX_GET_TEXTURE("2k_saturn.jpg", 1));
+  saturn->setShader(MX_GET_SHADER("blinn_phong"));
+  saturn->setTexture(MX_GET_TEXTURE("2k_saturn.jpg", 1));  
 
   MX::Node *rock = MX_SCENEGRAPH.search("Rock", MX_ROOT);
   rock->setTransform(MX::SCALE, 0.05f, 0);
   rock->setTransform(MX::BACKWARDS, 35.0f, 0);
   rock->setTransform(MX::LEFT, 0.5f, 0);
-  rock->setShader(MX_GET_SHADER("texture"));
+  rock->setShader(MX_GET_SHADER("phong"));
   rock->setTexture(MX_GET_TEXTURE("rock.jpg", 1));
 }
 
